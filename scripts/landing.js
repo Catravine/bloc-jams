100e5bc592abfeb3b6c5292f896f4319aee3f9ab
Caroline Courtney <caroline@carolinecourtney.com>
2016-07-23 11:29:40 -0400

    Assignment 22 Animation: fixed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,14 @@
 var animatePoints = function() {
-
   var points = document.getElementsByClassName('point');
 
-  var revealPoint = function (pointList) {
-    for(var i = 0; i < pointList.length; i++) {
-      pointList[i].style.opacity = 1;
-      pointList[i].style.transform = "scaleX(1) translateY(0)";
-      pointList[i].style.msTransform = "scaleX(1) translateY(0)";
-      pointList[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
 
-  revealPoint(points);
-
+  for(var i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
 };
