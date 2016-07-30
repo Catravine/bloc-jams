c950f1858f2852fbd5ca19fbfb02e18cf0fe8a81
Caroline Courtney <caroline@carolinecourtney.com>
2016-07-30 17:48:27 -0400

    Assignment 27 completed

--- scripts/album.js
+++ scripts/album.js
@@ -62,11 +62,20 @@ var setCurrentAlbum = function(album) {
 
 var findParentByClassName = function(element, targetClass) {
   if (element) {
-    var currentParent = element.parentElement;
-    while (currentParent.className != targetClass && currentParent.className !== null) {
-      currentParent = currentParent.parentElement;
+    if (element.parentElement === null) {
+      alert('No parent found');
+    } else {
+      var currentParent = element.parentElement;
+      while (currentParent.className != targetClass && currentParent.className !== null) {
+        if (currentParent.parentElement === null) {
+          alert('No parent found with that class name');
+          return;
+        } else {
+          currentParent = currentParent.parentElement;
+        }
+      }
+      return currentParent;
     }
-    return currentParent;
   }
 };
 
