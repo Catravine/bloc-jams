32e3c712854b05cf34c411ca23b324d256d283b4
Caroline Courtney <caroline@carolinecourtney.com>
2016-08-14 15:34:06 -0400

    Assignment 34 jQuery Seek Bars completed

--- scripts/album.js
+++ scripts/album.js
@@ -3,7 +3,7 @@ var createSongRow = function(songNumber, songName, songLength) {
       '<tr class="album-view-song-item">'
     + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     + ' <td class="song-item-title">' + songName + '</td>'
-    + ' <td class="song-item-duration">' + songLength + '</td>'
+    + ' <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
     + '</tr>';
 
   var $row = $(template);
@@ -89,6 +89,7 @@ var updateSeekBarWhileSongPlays = function() {
       var seekBarFillRatio = this.getTime() / this.getDuration();
       var $seekBar = $('.seek-control .seek-bar');
       updateSeekPercentage($seekBar, seekBarFillRatio);
+      setCurrentTimeInPlayerBar(filterTimeCode(seekBarFillRatio * this.getDuration()));
     });
   }
 };
@@ -179,6 +180,7 @@ var updatePlayerBarSong = function(){
   $('.currently-playing .artist-name').html(currentAlbum.artist);
   $('.currently-playing .artist-song-mobile').html(currentSongFromAlbum.title + ' - ' + currentAlbum.artist);
   $('.main-controls .play-pause').html(playerBarPauseButton);
+  setTotalTimeInPlayerBar(filterTimeCode(currentSongFromAlbum.duration));
 };
 
 var nextSong = function() {
@@ -280,6 +282,23 @@ var togglePlayFromPlayerBar = function() {
     currentSoundFile.play();
     getSongNumberCell(1).html(pauseButtonTemplate);
   }
+};
+
+var setCurrentTimeInPlayerBar = function(currentTime) {
+  $('.current-time').html(currentTime);
+};
+
+var setTotalTimeInPlayerBar = function(totalTime) {
+  $('.total-time').html(totalTime);
+};
+
+var filterTimeCode = function(timeInSeconds) {
+  var minutes = Math.floor(timeInSeconds / 60);
+  var seconds = Math.floor(timeInSeconds % 60);
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  return minutes + ':' + seconds;
 };
 
 $(document).ready(function() {
